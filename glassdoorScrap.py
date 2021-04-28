f9d62cf392ab8c248a14a557410b1a5dda093ee9
Rajat <jraj8407@gmail.com>
2021-04-28 10:33:23 +0530

    data added

--- glassdoorScrap.py
+++ glassdoorScrap.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 path = "F:\\chromedriver.exe"
 
-df = sc.get_jobs('data_scientist', 15, False, path, 15)
+df = sc.get_jobs('data_scientist', 1000, False, path, 15)
 
 df.to_csv('glassdoor_jobs.csv', index = False)
+
+
