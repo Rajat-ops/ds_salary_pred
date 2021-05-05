7c2bbc3af38d508b9bedcf74e5d0b37ca91b3ebb
Rajat <jraj8407@gmail.com>
2021-05-05 18:39:09 +0530

    Built the model for avg_salary

--- buildModel.py
+++ buildModel.py
@@ -26,10 +26,40 @@ print(model.fit().summary())
 
 """
 
-from sklearn.linear_model import LinearRegression
+"""
+//using LinearRegression
+
+from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.model_selection import cross_val_score
 
 lr = LinearRegression()
 lr.fit(X_train, y_train)
 
 print(cross_val_score(lr, X_train, y_train, scoring = "neg_mean_absolute_error", cv=3))
+
+lr_lasso = Lasso()
+print(np.mean(cross_val_score(lr_lasso, X_train, y_train, scoring = "neg_mean_absolute_error", cv=3)))
+"""
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
+
+rf_model = RandomForestRegressor(n_estimators=30)
+
+#print(cross_val_score(rf_model, X_train, y_train, scoring="neg_mean_absolute_error", cv=3))
+
+"""
+Tuning the Forest
+from sklearn.model_selection import GridSearchCV
+"""
+rf_model.fit(X_train, y_train)
+
+preds = rf_model.predict(X_test)
+
+#print(preds)
+
+from sklearn.metrics import mean_absolute_error
+
+print(mean_absolute_error(y_test, preds))
+
+
