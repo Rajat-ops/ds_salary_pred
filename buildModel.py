a4a182bd083cab4d51c67404ac0bf22243a48124
Rajat <jraj8407@gmail.com>
2021-05-05 08:35:24 +0530

    OLS and LinearRegression are implemented

---
+++ buildModel.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd 
+import statsmodels.api as sm
+
+df = pd.read_csv("salary_data_cleaned.csv")
+
+df_models = df[['Rating', 'Size', 'Type of ownership', 'Industry', 'Sector', 'Revenue', 'hourly', 'job_state', 'age', 'aws', 'excel','avg_salary', 'python_yn','spark']]
+
+df_dum = pd.get_dummies(df_models)
+
+from sklearn.model_selection import train_test_split
+
+X = df_dum.drop('avg_salary', axis=1)
+y = df_dum.avg_salary.values  
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
+
+"""
+// using OLS method
+
+X_sum = X = sm.add_constant(X)
+
+model = sm.OLS(y, X_sum)
+
+print(model.fit().summary())
+
+"""
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+print(cross_val_score(lr, X_train, y_train, scoring = "neg_mean_absolute_error", cv=3))
